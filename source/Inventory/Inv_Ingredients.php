513f53bf43e4d436c48c0129d7869804018abcbb
DantalianFree <mdrojo.chmsu@gmail.com>
2025-01-14 00:23:14 +0800

    changes

--- source/Inventory/Inv_Ingredients.php
+++ source/Inventory/Inv_Ingredients.php
@@ -71,7 +71,7 @@ $totalPages = ceil($total / $perPage);
 <body>
 <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#" id="navbarBrand">Logo</a>
+            <a class="navbar-brand" href="../admin/admin-dashboard.php" id="navbarBrand">Logo</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
